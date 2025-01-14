29a98a486e5a279219ee62dceaaf2c469d1066c9
Aayush <aayushsingh91255@gmail.com>
2025-01-14 18:49:50 +0530

    basic changes

--- chatapp/client/src/pages/auth/index.jsx
+++ chatapp/client/src/pages/auth/index.jsx
@@ -1,4 +1,13 @@
 const Auth = () => {
-  return <div>Auth</div>;
+  return (
+    <div className="h-[100vh] w-[100vw] flex items-center justify-center">
+      <div
+        className="h-[80vh] bg-white border-2
+      border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2"
+      >
+        <div></div>
+      </div>
+    </div>
+  );
 };
 export default Auth;
